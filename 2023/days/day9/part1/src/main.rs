2c083c4ac27f4d490eab68f01d01c5a081ceb4d1
Francesco Milano <milano.francesco@protonmail.com>
2023-12-09 12:17:26 +0100

    Added comments

--- 2023/days/day9/part1/src/main.rs
+++ 2023/days/day9/part1/src/main.rs
@@ -14,6 +14,9 @@ fn get_input_arr() -> Vec<String> {
     return arr;
 }
 
+/**
+ * Checks if the vector is made of all zeroes
+ */
 fn are_they_all_zeroes(integer_vec: &Vec<i32>) -> bool {
     for integer in integer_vec {
         if *integer != 0 {return false;}
@@ -21,6 +24,9 @@ fn are_they_all_zeroes(integer_vec: &Vec<i32>) -> bool {
     return true;
 }
 
+/**
+ * Given an integer vector return the "subtracted" version of it, i.e 1 2 3 4 -> 1 1 1
+ */
 fn get_subtracted_vec(integer_vec: Vec<i32>) -> Vec<i32> {
     let mut new_vec: Vec<i32> = Vec::new();
     if integer_vec.len() == 1 {
@@ -34,6 +40,11 @@ fn get_subtracted_vec(integer_vec: Vec<i32>) -> Vec<i32> {
     return new_vec;
 }
 
+
+/**
+ * Given a single vector of integers, return the predicted value at the end
+ * Recursive, of course
+ */
 fn get_predicted_value(integer_vec: Vec<i32>) -> i32 {
     if are_they_all_zeroes(&integer_vec) {
         return 0;
@@ -44,11 +55,13 @@ fn get_predicted_value(integer_vec: Vec<i32>) -> i32 {
 }
 
 fn main() {
+    //Input parsing
     let arr: Vec<String> = get_input_arr();
     let arr_str: Vec<&str> = arr.iter().map(|x| x.as_str()).collect();
 
     let mut sum_of_values = 0;
 
+    //for each line we get the "extrapolated" value
     for line in arr_str {
         let line_split: Vec<&str> = line.split(" ").collect();
         let integer_vec: Vec<i32> = line_split.iter().map(|x| x.parse::<i32>().unwrap()).collect();
