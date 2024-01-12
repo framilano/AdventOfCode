4e1ebcc043cb6dd77cd16c8cde57ab4859703f4a
framilano <milano.francesco@protonmail.com>
2024-01-12 01:34:31 +0100

    setup for day20

--- 2023/days/day20/part1/src/main.rs
+++ 2023/days/day20/part1/src/main.rs
@@ -1,3 +1,24 @@
+use std::process::exit;
+use std::fs;
+
+fn get_input_arr() -> Vec<String> {
+    let file = fs::read_to_string("../input");
+    if file.is_err() {
+        println!("File not found...");
+        exit(0);
+    }
+    let mut arr: Vec<String> = file.ok().unwrap().rsplit("\n").map(|x| String::from(x)).collect();
+    arr.reverse();
+    //Removing last empty line
+    arr.pop();
+    
+    return arr;
+}
+
 fn main() {
-    println!("Hello, world!");
+    //Input parsing
+    let arr: Vec<String> = get_input_arr();
+    let arr_str: Vec<&str> = arr.iter().map(|x| x.as_str()).collect();
+
+    print!("{:?}", arr_str);
 }
